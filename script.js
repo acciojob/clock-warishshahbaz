864ba33f87af6b3b957472628fb6bf6630bcc121
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-04 16:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function timerFn(){
+	let text = document.getElementById("timer");
+	let data = new Date();
+	text.innerHTML = date
+}
+setInterval(()=>{
+	timerFn()
+},1000);
+timerFn()
